f9e2470cda426420b5a56bdf2dd438846c29f3a8
jared <jaredspaulding1998@gmail.com>
2016-05-07 21:25:56 -0600

    Initial Commit

---
+++ android/src/com/spaulding/salt/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.spaulding.salt.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.spaulding.salt.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
