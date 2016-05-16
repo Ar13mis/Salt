ea3266a3a3e497fdd1758e55482bb0393911db8a
jared <jaredspaulding1998@gmail.com>
2016-05-16 16:01:29 -0600

    fixed typo

--- desktop/src/com/spaulding/salt/desktop/DesktopLauncher.java
+++ desktop/src/com/spaulding/salt/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.spaulding.salt.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Ladder";
+		config.title = "Salt";
 		config.useGL30 = false;
 		config.width = 1080;
 		config.height = 640;
